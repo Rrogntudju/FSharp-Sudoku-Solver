f67f3b102c5172fd084606f8f4bd910af192dae5
Mario Prince <mario.prince@outlook.com>
2017-02-08 13:40:23 -0500

    random_puzzle tail recursion plutôt que enumerate + mutable

--- Sudoku/SudokuSolver.fs
+++ Sudoku/SudokuSolver.fs
@@ -9,19 +9,19 @@ let center (s : string) (w : int) =
 
 let (>>=) m f = Option.bind f m
 
-let runWithUntil (v :  Option<'a>) (g : Option<'a> -> bool) (s : seq<'a -> Option<'a>>) : Option<'a> =
-    use enumerator = s.GetEnumerator ()
-    let mutable found = false
-    let mutable values = v
-    let mutable result = None
+let rec allSome (v :  Option<'a>) (sq : seq<'a -> Option<'a>>) : Option<'a> =
+    if Option.isNone v || Seq.isEmpty sq then
+        v
+    else
+      allSome (v >>= (Seq.head sq)) (Seq.tail sq)  
 
-    while enumerator.MoveNext () && not found do
-        result <- values >>= enumerator.Current    // run the current function with values and return the result
-        if g result then        // run the guard function on the result
-            found <- true
-        if Option.isSome result then
-            values <- result    // result becomes the next values if result is not None
-    result
+let firstSome (v :  Option<'a>) (sq : seq<'a -> Option<'a>>) : Option<'a> =
+    let rec firstSomeRec (v :  Option<'a>) (state : Option<'a>) (sq : seq<'a -> Option<'a>>) : Option<'a> =
+        if Option.isSome v || Seq.isEmpty sq then
+            v
+        else
+          firstSomeRec (state >>= (Seq.head sq)) state (Seq.tail sq)  
+    firstSomeRec None v sq
 
 let digits = "123456789" |> Seq.toList
 let rows = "ABCDEFGHI" |> Seq.toList
@@ -58,18 +58,17 @@ let grid_values (grid : string) : Option<HashMap<(char * char), char list>> =
     assert (chars.Length = 81)
     Some (HashMap [for z in List.zip squares chars -> z]) 
 
-
 let rec assign (s : char * char) (d : char) (values : HashMap<(char * char), char list>) : Option<HashMap<(char * char), char list>> =
     
     let rec eliminate (s : char * char) (d : char) (values : HashMap<(char * char), char list>) : Option<HashMap<(char * char), char list>> =
-    
+  
         let rule1  (values : HashMap<(char * char), char list>) : Option<HashMap<(char * char), char list>> =
         //  (1) If a square s is reduced to one value d', then eliminate d' from the peers.
             match values.[s].Length with
                 | 0 -> None         // Contradiction: removed last value
                 | 1 -> 
                     let d' = values.[s].[0]
-                    seq {for s' in peers.[s] -> eliminate s' d'} |> runWithUntil (Some values) Option.isNone
+                    seq {for s' in peers.[s] -> eliminate s' d'} |> allSome (Some values)
                 | _ -> Some values
 
         let rule2 (values : HashMap<(char * char), char list>) : Option<HashMap<(char * char), char list>> =
@@ -78,9 +77,9 @@ let rec assign (s : char * char) (d : char) (values : HashMap<(char * char), cha
                     let dplaces = [for s' in u do if d |> isIn values.[s'] then yield s']  
                     match dplaces.Length with
                         | 0 -> None  // Contradiction: no place for this value
-                        | 1 -> assign dplaces.[0] d v 
+                        | 1 -> assign dplaces.[0] d v   //  # d can only be in one place in unit; assign it there
                         | _ -> Some values
-            } |> runWithUntil (Some values) Option.isNone
+            } |> allSome (Some values)
 
     (*  Eliminate d from values[s]; propagate when values or places <= 2.
         Return Some values, except return None if a contradiction is detected. *)        
@@ -95,18 +94,17 @@ let rec assign (s : char * char) (d : char) (values : HashMap<(char * char), cha
     let other_values = values.[s] |> List.filter (fun d' -> d' <> d) 
     match other_values.Length with
         | 0 -> Some values      // Already assigned
-        | _ -> seq {for d' in other_values -> eliminate s d'} |> runWithUntil (Some values) Option.isNone
+        | _ -> seq {for d' in other_values -> eliminate s d'} |> allSome (Some values)
         | _ -> Some values
-
 
 let parse_grid (grid : string) : Option<HashMap<(char * char), char list>> =
 (*  Convert grid to Some dict of possible values, square, digits}, or
     return None if a contradiction is detected. *)
-        
     let assignGrid (gvalues : HashMap<(char * char), char list>)  =
         let values = HashMap [for s in squares do yield s, digits]
         seq {for s in squares do for d in gvalues.[s] do if d |> isIn digits then yield assign s d
-        } |> runWithUntil (Some values) Option.isNone
+        } |> allSome (Some values)
+    
     grid_values grid >>= assignGrid
 
 let rec search (values : HashMap<(char * char), char list>) : Option<HashMap<(char * char), char list>> =
@@ -116,8 +114,8 @@ let rec search (values : HashMap<(char * char), char list>) : Option<HashMap<(ch
     else
 //      Chose the unfilled square s with the fewest possibilities
         let _, s = seq {for s in squares do if values.[s].Length > 1 then yield values.[s].Length, s} |> Seq.min
-        seq {for d in values.[s] -> fun v -> assign s d v >>= search} |> runWithUntil (Some values) Option.isSome 
-
+        seq {for d in values.[s] -> fun v -> assign s d v >>= search} |> firstSome (Some values) 
+ 
 let solve (grid : string) : Option<HashMap<(char * char), char list>> = 
     grid |> parse_grid >>= search
 
@@ -127,10 +125,44 @@ let solved (values : HashMap<(char * char), char list>) : Option<HashMap<(char *
     match seq {for u in unitlist -> isUnitSolved u} |> Seq.forall (fun b -> b) with
         | true -> Some values
         | false -> None
+    
+let rg = System.Random()
+
+let rec random_puzzle (N : int) : string =
+(* Make a random puzzle with N or more assignments. Restart on contradictions.
+    Note the resulting puzzle is not guaranteed to be solvable, but empirically
+    about 99.8% of them are solvable. Some have multiple solutions.    *)
+    let shuffled (l : 'a list) : 'a list =
+       l |> List.permute (fun _ -> rg.Next(l.Length))
+  
+    let choice (l : 'a list) : 'a =
+        l.[rg.Next(l.Length)]
+    
+    let rec findPuzzle (values : Option<HashMap<(char * char), char list>>) (sq : seq<char * char>) : Option<HashMap<(char * char), char list>> =
+        match values with
+            | None -> None
+            | Some v -> 
+                let ds = [for s in squares do if v.[s].Length = 1 then yield v.[s]]
+                if ds.Length >= N && (List.distinct ds).Length >= 8 then
+                    values
+                else
+                    if Seq.isEmpty sq then
+                        None
+                    else
+                        let s = Seq.head sq
+                        let values' = assign s (v.[s] |> choice) v
+                        findPuzzle values' (Seq.tail sq)
+ 
+    let values = HashMap [for s in squares do yield s, digits]
+    let values' = seq {for s in shuffled squares -> s} |> findPuzzle (Some values)
+    match values' with
+        | None -> random_puzzle N
+        | Some v -> String.concat "" [for s in squares -> if v.[s].Length = 1 then v.[s].ToString() else "."]
 
 let solve_all (grids : seq<string>) (name) (showif : float Option) : unit =
-//  Attempt to solve a sequence of grids. Report results.
-//  When showif is a number of seconds, display puzzles that take longer.
+(*  Attempt to solve a sequence of grids. Report results.
+    When showif is a number of seconds, display puzzles that take longer.
+    When showif is None, don't display any puzzles.     *)
     let time_solved grid showif =
         let timer = new System.Diagnostics.Stopwatch()
         timer.Start()
@@ -151,10 +183,11 @@ let solve_all (grids : seq<string>) (name) (showif : float Option) : unit =
                 (float N / (times |> List.sum)) (times |> List.max)
 [<EntryPoint>]
 let main argv = 
-    solve_all (File.ReadLines "top95.txt") "hard" (Some 0.5)
+    solve_all (File.ReadLines "easy50.txt") "easy" None
+    solve_all (File.ReadLines "top95.txt") "hard" None
     solve_all (File.ReadLines "hardest.txt") "hardest" None
-
+//    solve_all ([for _ in [1 .. 99] -> random_puzzle 17]) "random" (Some 100.0)
     System.Console.Read() |> ignore
 
-    0 // retourne du code de sortie entier
+    0
 
